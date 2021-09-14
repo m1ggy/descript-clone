43e1df7a35b3c65acaad829457e4087846854942
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-09-14 13:12:35 +0800

    add custom hook for recording, minor change to RecordVideoModal

--- client/src/components/RecordVideoModal.js
+++ client/src/components/RecordVideoModal.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, Accordion, Alert } from 'react-bootstrap';
 import useRecorder from '../hooks/useRecorder';
 import CountDown from './CountDown';
 import OverlayToolTip from './OverlayToolTip';
 import { FaQuestionCircle } from 'react-icons/fa';
+import { IoSettingsOutline } from 'react-icons/io5';
+import useStore from '../store';
+import useProject from '../hooks/useProject';
+
 function RecordVideoModal({ show, setShow }) {
   const [options, setOptions] = useState({ audio: false, video: true });
   const [recording, setRecording] = useState(false);
   const [showCountdown, setShowCountdown] = useState(false);
-  const [showPref, setShowPref] = useState(true);
+  const [proceed, setProceed] = useState(false);
+  const [recordingFinished, setRecordingFinished] = useState(false);
+  const [projectName, setProjectName] = useState('');
+  const [message, setMessage] = useState({ content: '', type: '' });
+  const [serverMessage, setServerMessage] = useState({ type: '', content: '' });
+  const [loading, setLoading] = useState(false);
+
+  const { createProject, getProjects, uploadMediaProject } = useProject();
+  const projects = useStore((state) => state.projects);
+  const unsub = useStore.subscribe(console.log, (state) => state.project);
+
   const preview = useRef();
   const { startRecording, stopRecording, url, recordingActive } = useRecorder(
     { audio: true, video: true },
@@ -17,9 +31,60 @@ function RecordVideoModal({ show, setShow }) {
     true
   );
   const [delay, setDelay] = useState(3);
+
+  ///local fucntions
   const handleClose = () => {
     setShow(!show);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      setServerMessage({
+        content: await createProject(projectName),
+        type: 'success',
+      });
+    } catch (e) {
+      console.log(e);
+      setServerMessage({ content: e, type: 'danger' });
+    }
+    setProjectName('');
+    setMessage({ type: '', content: '' });
+    setServerMessage({ type: '', content: '' });
+
+    await getProjects();
+
+    console.log(projects);
+    unsub();
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (projectName === '') {
+      return;
+    } else if (projectName.length < 5 || projectName.length > 20) {
+      return setMessage({
+        type: 'danger',
+        content: 'Project name should be 5 characters to 20 characters long!',
+      });
+    } else {
+      const filtered = projects.filter(
+        (x) => x.projectName === projectName.trim()
+      );
+
+      if (filtered.length) {
+        return setMessage({
+          type: 'danger',
+          content: `You already have a project named ${projectName}`,
+        });
+      }
+      setMessage({
+        type: 'success',
+        content: `${projectName} is available.`,
+      });
+    }
+  }, [projectName, projects]);
 
   useEffect(() => {
     console.log(url);
@@ -29,152 +94,219 @@ function RecordVideoModal({ show, setShow }) {
     setRecording(recordingActive);
   }, [recordingActive]);
 
+  useEffect(() => {
+    console.log(options.audio);
+  }, [options]);
+
   return (
     <Modal show={show} onHide={handleClose} centered size='lg'>
       <Modal.Header closeButton>
         <Modal.Title>New Project from Video Recording</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form
-          as='div'
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'flex-start',
-            }}
-          >
-            <Button
-              variant='success'
-              onClick={() => {
-                startRecording(delay * 1000, options.audio, setShowCountdown);
-              }}
-              style={{ width: 'fit-content' }}
-              disabled={recording}
-              size='sm'
-            >
-              Start Recording 🔴
-            </Button>
-            <Button
-              variant='danger'
-              size='sm'
-              style={{ width: 'fit-content' }}
-              onClick={() => stopRecording()}
-              disabled={!recording}
-            >
-              Stop Recording 🟥
-            </Button>
-          </div>
-          <div
-            style={{ display: 'flex', marginTop: '15px', marginBottom: '10px' }}
-          >
-            <h4>Preferences</h4>
-            <pre onClick={() => setShowPref(!showPref)}>
-              {showPref ? 'Hide' : 'Show'}
-            </pre>
-          </div>
-
-          {showPref && (
-            <>
-              <Form.Group style={{ textAlign: 'center' }}>
-                <Form.Label
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                >
-                  Delay
-                  <OverlayToolTip
-                    placement='bottom'
-                    content={
-                      <p>Adjust the delay before starting the recording.</p>
-                    }
-                  >
-                    <div>
-                      <FaQuestionCircle size='1em' />
-                    </div>
-                  </OverlayToolTip>
-                </Form.Label>
-
+        {proceed ? (
+          <>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>Project Name</Form.Label>
                 <Form.Control
-                  inline
-                  value={delay}
-                  onChange={(e) => {
-                    setDelay(e.target.value);
-                  }}
-                  type='number'
-                  max={10}
-                  min={3}
+                  type='text'
+                  value={projectName}
+                  onChange={(e) => setProjectName(e.target.value)}
+                  isInvalid={message.type === 'danger' && true}
+                  isValid={message.type === 'success' && true}
                 />
+                <Form.Control.Feedback
+                  type={message.type === 'success' ? 'valid' : 'invalid'}
+                >
+                  {message.content}
+                </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group
+              <Button disabled={loading} type='submit'>
+                {loading ? 'Loading ...' : 'Submit'}
+              </Button>
+            </Form>
+            {serverMessage.content !== '' && (
+              <Alert variant={serverMessage.type}>
+                {serverMessage.content}
+              </Alert>
+            )}
+          </>
+        ) : (
+          <>
+            <Form
+              as='div'
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+              }}
+            >
+              <div
                 style={{
-                  textAlign: 'center',
                   display: 'flex',
                   justifyContent: 'center',
+                  alignItems: 'flex-start',
                 }}
               >
-                <Form.Label
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                >
-                  Capture Mic?
-                  <OverlayToolTip
-                    placement='bottom'
-                    content={
-                      <p>
-                        Enable recording of audio from mic input. make sure you
-                        have a mic connected!
-                      </p>
-                    }
-                  >
-                    <div>
-                      <FaQuestionCircle size='1em' />
-                    </div>
-                  </OverlayToolTip>
-                </Form.Label>
-                <Form.Check
-                  inline
-                  value={options.audio}
-                  onChange={(e) => {
-                    setOptions({
-                      ...options,
-                      audio: e.target.checked,
-                    });
+                <Button
+                  variant='success'
+                  onClick={() => {
+                    startRecording(
+                      delay * 1000,
+                      options.audio,
+                      setShowCountdown,
+                      options.audio,
+                      setRecordingFinished
+                    );
                   }}
-                  style={{ marginLeft: '10px' }}
+                  style={{ width: 'fit-content' }}
+                  disabled={recording}
+                  size='sm'
+                >
+                  Start Recording 🔴
+                </Button>
+                <Button
+                  variant='danger'
+                  size='sm'
+                  style={{ width: 'fit-content' }}
+                  onClick={() => stopRecording()}
+                  disabled={!recording}
+                >
+                  Stop Recording 🟥
+                </Button>
+              </div>
+              <Accordion
+                style={{ marginTop: '15px' }}
+                defaultActiveKey='0'
+                flush
+              >
+                <Accordion.Item eventKey='0'>
+                  <Accordion.Header>
+                    Preferences {'  '}
+                    <IoSettingsOutline
+                      size='1.2em'
+                      style={{ marginLeft: '5px' }}
+                    />
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div>
+                      <Form.Group style={{ textAlign: 'center' }}>
+                        <Form.Label
+                          style={{ display: 'flex', justifyContent: 'center' }}
+                        >
+                          Delay
+                          <OverlayToolTip
+                            placement='bottom'
+                            content={
+                              <p>
+                                Adjust the delay before starting the recording.
+                              </p>
+                            }
+                          >
+                            <div>
+                              <FaQuestionCircle size='1em' />
+                            </div>
+                          </OverlayToolTip>
+                        </Form.Label>
+
+                        <Form.Control
+                          value={delay}
+                          onChange={(e) => {
+                            setDelay(e.target.value);
+                          }}
+                          type='number'
+                          max={10}
+                          min={3}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        style={{
+                          textAlign: 'center',
+                          display: 'flex',
+                          justifyContent: 'center',
+                        }}
+                      >
+                        <Form.Label
+                          style={{ display: 'flex', justifyContent: 'center' }}
+                        >
+                          Capture Mic?
+                          <OverlayToolTip
+                            placement='bottom'
+                            content={
+                              <p>
+                                Enable recording of audio from mic input. make
+                                sure you have a mic connected!
+                              </p>
+                            }
+                          >
+                            <div>
+                              <FaQuestionCircle size='1em' />
+                            </div>
+                          </OverlayToolTip>
+                        </Form.Label>
+                        <Form.Check
+                          inline
+                          value={options.audio}
+                          onChange={(e) => {
+                            setOptions({
+                              ...options,
+                              audio: e.target.checked,
+                            });
+                          }}
+                          style={{ marginLeft: '10px' }}
+                        />
+                      </Form.Group>
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Form>
+
+            {showCountdown && (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  marginTop: '25px',
+                }}
+              >
+                <CountDown delay={delay} />
+              </div>
+            )}
+
+            {recording && (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  marginTop: '25px',
+                }}
+              >
+                <h3>Recording now....</h3>
+              </div>
+            )}
+            {url && (
+              <div style={{ marginTop: '25px' }}>
+                <h4>Preview</h4>
+                <video
+                  ref={preview}
+                  controls
+                  style={{ width: '100%', height: '100%' }}
                 />
-              </Form.Group>
-            </>
-          )}
-        </Form>
-        {showCountdown && (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginTop: '25px',
-            }}
-          >
-            <CountDown delay={delay} />
-          </div>
+              </div>
+            )}
+          </>
         )}
-        {recording && <h3>Recording now....</h3>}
-        {url && (
-          <div style={{ marginTop: '25px' }}>
-            <h4>Preview</h4>
-            <video
-              ref={preview}
-              controls
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
-        )}
+        <Form></Form>
       </Modal.Body>
 
       <Modal.Footer>
+        {recordingFinished && (
+          <Button onClick={() => setProceed(true)} variant='success'>
+            Proceed
+          </Button>
+        )}
         <Button variant='danger' onClick={() => setShow(false)}>
           Cancel
         </Button>
--- client/src/hooks/useRecorder.js
+++ client/src/hooks/useRecorder.js
@@ -4,19 +4,33 @@ let captureStream = null;
 let audioStream = null;
 let recording = null;
 let data = [];
+let a2 = null;
+let a1 = null;
+let audio1 = null;
+let audio2 = null;
+let tracks = null;
+let combinedAudio = null;
+let destination = null;
 
 const useRecorder = (
-  screenOptions = { audio: false, video: true },
+  screenOptions = { audio: true, video: true },
   mimeTypes = { audio: 'audio/webm', video: 'video/webm; codec=vp9' },
   video = true
 ) => {
   const [url, setUrl] = useState('');
   const [recordingActive, setRecordingActive] = useState(false);
 
-  async function startRecording(delay, audio = true, setCountdown) {
-    ///check if user wants with audio
+  async function startRecording(
+    delay,
+    audio = true,
+    setCountdown,
+    enableAudio,
+    finished
+  ) {
+    finished(false);
     setUrl('');
     data = [];
+    ///check if user wants with audio
     if (audio) {
       try {
         audioStream = await navigator.mediaDevices.getUserMedia({
@@ -35,8 +49,45 @@ const useRecorder = (
         console.log(e);
       }
     }
+
     if (captureStream && audioStream) {
+      ///TODO: refactor logic
       captureStream.addTrack(audioStream.getTracks()[0]);
+
+      tracks = captureStream.getAudioTracks();
+      const audioContext = new AudioContext();
+
+      a1 = new MediaStream([tracks[0]], { mimeType: 'audio/webm' });
+      if (tracks.length > 1) {
+        a2 = new MediaStream([tracks[1]], { mimeType: 'audio/webm' });
+      }
+
+      audio1 = audioContext.createMediaStreamSource(a1);
+      if (tracks.length > 1) {
+        audio2 = audioContext.createMediaStreamSource(a2);
+      }
+
+      console.log(audio1.mediaStream.getAudioTracks());
+      destination = audioContext.createMediaStreamDestination();
+
+      audio1.connect(destination);
+      if (tracks.length > 1) {
+        audio2.connect(destination);
+      }
+
+      combinedAudio = new MediaRecorder(destination.stream, {
+        mimeType: 'audio/webm',
+      });
+
+      console.log('Audio:', combinedAudio.stream.getTracks());
+      console.log('Video:', captureStream.getTracks());
+
+      tracks.forEach((track) => {
+        captureStream.removeTrack(track);
+      });
+
+      captureStream.addTrack(combinedAudio.stream.getTracks()[0]);
+      destination = null;
     }
     if (captureStream) {
       recording = new MediaRecorder(captureStream, {
@@ -45,16 +96,28 @@ const useRecorder = (
 
       recording.onstop = () => {
         ///stop all streams
+        console.log(captureStream.getTracks());
         captureStream.getTracks().forEach((track) => {
           track.stop();
         });
+        if (enableAudio) {
+          combinedAudio.stream.getTracks().forEach((t) => t.stop());
+          // stop tracks stream
+
+          //stop temp mediarecorders tracks
+          a1.getTracks().forEach((t) => t.stop());
+
+          if (tracks.length > 1) {
+            a2.getTracks().forEach((t) => t.stop());
+          }
+        }
 
         ///create new blob
         const blob = new Blob(data, { type: mimeTypes.video });
         ///set the new url
         setUrl(URL.createObjectURL(blob));
         setRecordingActive(false);
-
+        finished(true);
         ///reset media variables
         recording = null;
         captureStream = null;
